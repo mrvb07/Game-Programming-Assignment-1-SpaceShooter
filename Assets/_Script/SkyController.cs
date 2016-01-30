dab880020be77e02d698dc3ea6d3d5a2d7c52266
Vinay Bhardwaj <Vinay Bhardwaj>
2016-01-30 18:31:26 -0500

    Added 1st Script

---
+++ Assets/_Script/SkyController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyController : MonoBehaviour {
+    //PRIVATE INSTANCE VARIABLES
+    private Transform _transform;
+
+	// Use this for initialization
+	void Start () {
+        this._transform = gameObject.GetComponent<Transform>();
+        this._transform.position = new Vector2(850f, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
