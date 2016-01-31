69871706f121aadb1fb0512956974f918007f306
Vinay Bhardwaj <Vinay Bhardwaj>
2016-01-30 20:25:24 -0500

    Updated Background

--- Assets/_Script/SkyController.cs
+++ Assets/_Script/SkyController.cs
@@ -4,15 +4,38 @@ using System.Collections;
 public class SkyController : MonoBehaviour {
     //PRIVATE INSTANCE VARIABLES
     private Transform _transform;
+    private Vector2 _currentPosition;
+
+    //PUBLIC INSTANCE VARIABLES
+    public float speed = 5f;
+
 
 	// Use this for initialization
 	void Start () {
         this._transform = gameObject.GetComponent<Transform>();
-        this._transform.position = new Vector2(850f, 0f);
+        //Reset the background
+        this.reset();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        this._currentPosition = this._transform.position;
+        this._currentPosition -= new Vector2(this.speed, 0);
+        this._transform.position = this._currentPosition;
+
+        if (this._currentPosition.x <= 129)
+        {
+            this.reset();
+        }
 	}
+
+    public void reset()
+    {
+        this._transform.position = new Vector2(783f, 0f);
+    }
+
+    public void currentPosition()
+    {
+
+    }
 }
